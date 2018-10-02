a7b64a2ab59819100f8cb67fa5e3f7b2187fd2d8
Arpita Chowdhury <bsse0724@iit.du.ac.bd>
2018-10-02 21:02:23 +0600

    Completed Front Page Design
    *intgrated bootstrap
    *integrated jquery
    *create front page with customized button

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/visualizer")
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
--- static/in_it.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-  let x = document.getElementById("mySelect").value;
-  document.getElementById("demo").innerHTML = "You selected: " + x;
-}
